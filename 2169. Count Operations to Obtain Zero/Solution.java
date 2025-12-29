d53a0ba77a5d1420b127af371322c410420c29d6
Shivansh Pratap Singh <shivansh.pratap.singh8736@gmail.com>
2025-12-29 23:24:28 +0530

    Time: 1 ms (62.29%) | Memory: 46.26 MB (38.92%) - LeetSync

---
+++ 2169. Count Operations to Obtain Zero/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int countOperations(int num1, int num2) {
+        int op=0;
+       while(num1!=0 && num2!=0){ 
+        op++;
+        if(num1>=num2)
+        num1-=num2;
+        else
+        num2-=num1;
+        }
+        return op;
+    }
+}
