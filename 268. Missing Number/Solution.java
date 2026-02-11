f7398399ca7c7122b1e7dca746340db532b18ea1
Shivansh Pratap Singh <shivansh.pratap.singh8736@gmail.com>
2026-02-11 09:15:37 +0530

    Solution.java

---
+++ 268. Missing Number/Solution.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+class Solution {
+    public int missingNumber(int[] nums) {
+    Arrays.sort(nums);
+    int i=0;
+    while(i<nums.length){
+        if(nums[i]!=i)
+        return i;
+        i++;
+
+    }
+    return i;
+         
+    }
+}
