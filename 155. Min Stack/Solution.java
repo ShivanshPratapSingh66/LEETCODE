30b8e395155b689e4769d02d18b720a7b4efeaa7
Shivansh Pratap Singh <shivansh.pratap.singh8736@gmail.com>
2026-02-13 00:21:51 +0530

    Solution.java

---
+++ 155. Min Stack/Solution.java
@@ -0,0 +1,38 @@
+class MinStack {
+    ArrayList<Integer> list =new ArrayList<>();
+    public MinStack() {
+        
+    }
+    
+    public void push(int val) {
+        list.add(val);
+    }
+    
+    public void pop() {
+        if(!list.isEmpty())
+        list.remove(list.size()-1);
+        
+    }
+    
+    public int top() {
+      
+       return list.get(list.size()-1);
+    }
+    
+    public int getMin() {
+        int min=Integer.MAX_VALUE;
+        for(int i=0;i<list.size();i++){
+        min=Math.min(min,list.get(i));
+        }
+        return min;
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
